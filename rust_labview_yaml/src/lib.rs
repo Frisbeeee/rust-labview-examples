db2b7d55255e31d28809ec6d7380855acee1fa9a
Cameron Frisby <cameron.frisby@ni.com>
2020-04-29 15:44:57 -0500

    Apply Rust style guidelines with rustfmt

--- rust_labview_yaml/src/lib.rs
+++ rust_labview_yaml/src/lib.rs
@@ -4,7 +4,7 @@ extern crate yaml_rust;
 use libc::c_char;
 use std::ffi::{CStr, CString};
 use std::fs;
-use yaml_rust::{YamlLoader};
+use yaml_rust::YamlLoader;
 
 #[cfg(test)]
 mod tests {
@@ -12,9 +12,12 @@ mod tests {
     use std::ffi::CString;
 
     #[test]
-    fn c_load_unload_no_panic(){
-        let c_string_path = CString::new(String::from("C:/sandbox/rust-labview/rust_labview_yaml/test_configuration.yml")).unwrap();
-        let c_string_ptr = c_string_path.as_ptr(); 
+    fn c_load_unload_no_panic() {
+        let c_string_path = CString::new(String::from(
+            "C:/sandbox/rust-labview/rust_labview_yaml/test_configuration.yml",
+        ))
+        .unwrap();
+        let c_string_ptr = c_string_path.as_ptr();
 
         let daq_configs_ptr = load_daq_configurations(c_string_ptr);
 
@@ -22,7 +25,7 @@ mod tests {
     }
 
     #[test]
-    fn c_three_configurations_load(){
+    fn c_three_configurations_load() {
         let daq_configs_ptr = load_config_test_setup();
 
         let count = get_configuration_count(daq_configs_ptr);
@@ -31,38 +34,38 @@ mod tests {
     }
 
     #[test]
-    fn c_get_min_voltage(){
+    fn c_get_min_voltage() {
         let daq_configs_ptr = load_config_test_setup();
-        
+
         let min = get_min_voltage(daq_configs_ptr, 0);
         assert_eq!(min, -5);
         unload_daq_configurations(daq_configs_ptr);
     }
 
     #[test]
-    fn c_get_max_voltage(){
+    fn c_get_max_voltage() {
         let daq_configs_ptr = load_config_test_setup();
-        
+
         let max = get_max_voltage(daq_configs_ptr, 2);
         assert_eq!(max, 1);
         unload_daq_configurations(daq_configs_ptr);
     }
 
     #[test]
-    fn c_get_sample_rate(){
+    fn c_get_sample_rate() {
         let daq_configs_ptr = load_config_test_setup();
-        
+
         let rate = get_sample_rate_hz(daq_configs_ptr, 1);
         assert_eq!(rate, 1000);
         unload_daq_configurations(daq_configs_ptr);
     }
 
-     #[test]
-    fn c_get_hardware(){
+    #[test]
+    fn c_get_hardware() {
         let daq_configs_ptr = load_config_test_setup();
-        
+
         let returned_chars = get_hardware_channels(daq_configs_ptr, 2);
-        unsafe{
+        unsafe {
             let c_hardware_string = CString::from_raw(returned_chars);
             let hardware_channels = c_hardware_string.to_str().unwrap();
             assert_eq!(hardware_channels, "cDAQ1Mod3/ai0:15");
@@ -70,9 +73,12 @@ mod tests {
         unload_daq_configurations(daq_configs_ptr);
     }
 
-    fn load_config_test_setup() -> *mut DaqConfigurations{
-        let c_string_path = CString::new(String::from("C:/sandbox/rust-labview/rust_labview_yaml/test_configuration.yml")).unwrap();
-        let c_string_ptr = c_string_path.as_ptr(); 
+    fn load_config_test_setup() -> *mut DaqConfigurations {
+        let c_string_path = CString::new(String::from(
+            "C:/sandbox/rust-labview/rust_labview_yaml/test_configuration.yml",
+        ))
+        .unwrap();
+        let c_string_ptr = c_string_path.as_ptr();
         load_daq_configurations(c_string_ptr)
     }
 }
@@ -91,37 +97,37 @@ impl DaqConfigurations {
         }
     }
 
-    fn load_configuration_file(&mut self, filepath: String){
+    fn load_configuration_file(&mut self, filepath: String) {
         let file_contents = fs::read_to_string(filepath).unwrap();
         let docs = YamlLoader::load_from_str(&file_contents).unwrap();
         let doc = &docs[0];
         self.configs = doc["cDAQ_configurations"].as_vec().unwrap().to_vec();
     }
 
-    fn get_count(& self) -> u32 {
+    fn get_count(&self) -> u32 {
         let length = self.configs.len();
         length as u32
     }
 
-    fn get_min(& self, index: u32) -> i64{
+    fn get_min(&self, index: u32) -> i64 {
         let index = index as usize;
         let config = &self.configs[index];
         config["min_voltage"].as_i64().unwrap()
     }
 
-    fn get_max(& self, index: u32) -> i64{
+    fn get_max(&self, index: u32) -> i64 {
         let index = index as usize;
         let config = &self.configs[index];
         config["max_voltage"].as_i64().unwrap()
     }
 
-    fn get_rate(& self, index: u32) -> i64{
+    fn get_rate(&self, index: u32) -> i64 {
         let index = index as usize;
         let config = &self.configs[index];
         config["sample_rate_Hz"].as_i64().unwrap()
     }
 
-    fn get_hw(& self, index: u32) -> String{
+    fn get_hw(&self, index: u32) -> String {
         let index = index as usize;
         let config = &self.configs[index];
         config["hardware_channels"].as_str().unwrap().to_string()
@@ -129,7 +135,7 @@ impl DaqConfigurations {
 }
 
 #[no_mangle]
-pub extern "C" fn load_daq_configurations(filepath: *const c_char) -> *mut DaqConfigurations{
+pub extern "C" fn load_daq_configurations(filepath: *const c_char) -> *mut DaqConfigurations {
     let filepath = unsafe {
         assert!(!filepath.is_null());
         CStr::from_ptr(filepath)
@@ -152,7 +158,7 @@ pub extern "C" fn unload_daq_configurations(ptr: *mut DaqConfigurations) {
 
 #[no_mangle]
 pub extern "C" fn get_configuration_count(ptr: *mut DaqConfigurations) -> u32 {
-    let configs = unsafe{
+    let configs = unsafe {
         assert!(!ptr.is_null());
         &mut *ptr
     };
@@ -161,7 +167,7 @@ pub extern "C" fn get_configuration_count(ptr: *mut DaqConfigurations) -> u32 {
 
 #[no_mangle]
 pub extern "C" fn get_min_voltage(ptr: *mut DaqConfigurations, index: u32) -> i64 {
-    let configs = unsafe{
+    let configs = unsafe {
         assert!(!ptr.is_null());
         &mut *ptr
     };
@@ -170,7 +176,7 @@ pub extern "C" fn get_min_voltage(ptr: *mut DaqConfigurations, index: u32) -> i6
 
 #[no_mangle]
 pub extern "C" fn get_max_voltage(ptr: *mut DaqConfigurations, index: u32) -> i64 {
-    let configs = unsafe{
+    let configs = unsafe {
         assert!(!ptr.is_null());
         &mut *ptr
     };
@@ -179,7 +185,7 @@ pub extern "C" fn get_max_voltage(ptr: *mut DaqConfigurations, index: u32) -> i6
 
 #[no_mangle]
 pub extern "C" fn get_sample_rate_hz(ptr: *mut DaqConfigurations, index: u32) -> i64 {
-    let configs = unsafe{
+    let configs = unsafe {
         assert!(!ptr.is_null());
         &mut *ptr
     };
@@ -188,7 +194,7 @@ pub extern "C" fn get_sample_rate_hz(ptr: *mut DaqConfigurations, index: u32) ->
 
 #[no_mangle]
 pub extern "C" fn get_hardware_channels(ptr: *mut DaqConfigurations, index: u32) -> *mut c_char {
-    let configs = unsafe{
+    let configs = unsafe {
         assert!(!ptr.is_null());
         &mut *ptr
     };
